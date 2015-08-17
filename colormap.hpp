f002dee4871c12b393d4f02c28648a9bdadb9957
Yuzo <hyzgog@gmail.com>
2015-08-17 16:34:31 +0800

    add Lab mode

--- colormap.hpp
+++ colormap.hpp
@@ -17,9 +17,21 @@ Vec3b RGB2HSV(Vec3b rgb) {
 	return m.at<Vec3b>(0, 0);
 }
 
+Vec3b RGB2Lab(Vec3b rgb) {
+	Mat m(Size(1, 1), CV_8UC3, Scalar(rgb));
+	cvtColor(m, m, CV_RGB2Lab);
+	return m.at<Vec3b>(0, 0);
+}
+
 Vec3b HSV2RGB(Vec3b hsv) {
 	Mat m(Size(1, 1), CV_8UC3, Scalar(hsv));
 	cvtColor(m, m, CV_HSV2RGB);
+	return m.at<Vec3b>(0, 0);
+}
+
+Vec3b Lab2RGB(Vec3b lab) {
+	Mat m(Size(1, 1), CV_8UC3, Scalar(lab));
+	cvtColor(m, m, CV_Lab2RGB);
 	return m.at<Vec3b>(0, 0);
 }
 
@@ -37,6 +49,21 @@ Vec3i diffHSV2RGB(Vec3b referRGB, Vec3b srcRGB, Vec3b destRGB) {
 	Vec3i diffRGB = Vec3i(newRGB) - Vec3i(referRGB);
 
 	cout << boost::format("referRGB: %1%, referHSV: %2%, srcRGB: %3%, srcHSV %4%, destRGB: %5%, destHSV: %6%, newRGB: %7%, newHSV: %8%, diffRGB: %9%, diffHSV: %10%") % referRGB % referHSV % srcRGB % srcHSV % destRGB % destHSV % newRGB % newHSV % diffRGB % diffHSV << endl;
+	return diffRGB;
+}
+
+Vec3i diffLab2RGB(Vec3b referRGB, Vec3b srcRGB, Vec3b destRGB) {
+	Vec3b referLab = RGB2Lab(referRGB);
+	Vec3b srcLab = RGB2Lab(srcRGB);
+	Vec3b destLab = RGB2Lab(destRGB);
+
+	Vec3i diffLab = Vec3i(destLab) - Vec3i(srcLab);
+	Vec3i newLab = Vec3i(referLab) + diffLab;
+
+	Vec3b newRGB = Lab2RGB(Vec3b(newLab));
+	Vec3i diffRGB = Vec3i(newRGB) - Vec3i(referRGB);
+
+	cout << boost::format("referRGB: %1%, referLab: %2%, srcRGB: %3%, srcLab %4%, destRGB: %5%, destLab: %6%, newRGB: %7%, newLab: %8%, diffRGB: %9%, diffLab: %10%") % referRGB % referLab % srcRGB % srcLab % destRGB % destLab % newRGB % newLab % diffRGB % diffLab << endl;
 	return diffRGB;
 }
 
